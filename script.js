2a87872f44960f0552295ee201f7a9f5a4ac3c70
Malay Thakur <malaythakur13@gmail.com>
2024-01-15 02:43:48 +0530

    Implemented dynamic product card creation and rendering based on data from the products object

--- script.js
+++ script.js
@@ -59,3 +59,20 @@ let products = {
         },
     ],
 };
+
+for (let i of products.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category
+    card.classList.add("card", "i.category");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+    //img tag
+    let image = document.createElement("img");
+    imgContainer.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+
+    document.getElementById("products").appendChild(card);
+}
